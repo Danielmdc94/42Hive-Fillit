0280d717d038f709d387e4c4d3f42175448091de
Daniel Palacio <dpalacio@student.hive.fi>
2022-01-26 16:56:21 +0200

    convert_file.c modified to comply with new structure

--- convert_file.c
+++ convert_file.c
@@ -6,16 +6,55 @@
 /*   By: dpalacio <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 15:46:01 by dpalacio          #+#    #+#             */
-/*   Updated: 2022/01/26 15:53:57 by dpalacio         ###   ########.fr       */
+/*   Updated: 2022/01/26 16:55:27 by dpalacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static int	validate_file(char *buff, int pieces)
+static int	validate_file(char *buff, int ret, int pieces)
 {
-	buff = NULL;
-	pieces =0;
+	int	i;
+	int	x;
+	int	y;
+	int	blocks;
+	pieces = 0;
+	i = 0;
+	x = 0;
+	y = 0;
+	blocks = 0;
+	while (i < ret)
+	{
+		while (y < 4)
+		{
+			while (x < 4)
+			{
+				if (buff[i] != '.' && buff[i] != '#')
+					error_manager(0);
+				if (buff[i] == '#')
+				{
+					blocks++;
+					if (buff[i] != buff[i - 1] && buff[i] != buff[i + 1]
+						&& buff[i] != buff[i - 5] && buff[i] != buff [i + 5])
+						error_manager(0);
+				}
+				x++;
+				i++;
+			}
+			if (buff[i] != '\n')
+				error_manager(0);
+			x = 0;
+			i++;
+			y++;
+		}
+		if (buff[i] != '\n' && buff[i] != '\0')
+			error_manager(0);
+		y = 0;
+		if (blocks != 4)
+			error_manager(0);
+		blocks = 0;
+		i++;
+	}
 	return (1);
 }
 
@@ -35,6 +74,6 @@ int	convert_file(char *file)
 	if ((ret + 1) % 21 != 0 || ret == 0)
 		error_manager(0);
 	pieces = (ret + 1) / 21;
-	validate_file(buff, pieces);
+	validate_file(buff, ret, pieces);
 	return (1);
 }
