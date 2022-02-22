716f961f1ae5a186680182de52ec1a9f7d9a242a
Daniel Palacio <dpalacio@student.hive.fi>
2022-02-22 11:54:32 +0200

    .h changed

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: dpalacio <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 13:54:47 by dpalacio          #+#    #+#             */
-/*   Updated: 2022/02/10 18:29:36 by dpalacio         ###   ########.fr       */
+/*   Updated: 2022/02/17 16:57:29 by dpalacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@
 void	error(int error_code);
 int		main(int argc, char **argv);
 char	*read_file(char *file);
+int		convert_file(char *file);
+
 
 #endif
