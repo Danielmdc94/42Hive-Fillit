281f50bf5f49a74bd00e7bc360bf4daebcba413c
Daniel Palacio <dpalacio@student.hive.fi>
2022-03-07 14:51:12 +0200

    fixed my own mess :)

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 /*   By: dpalacio <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 13:59:00 by dpalacio          #+#    #+#             */
-/*   Updated: 2022/03/04 13:02:08 by dpalacio         ###   ########.fr       */
+/*   Updated: 2022/03/07 14:46:43 by dpalacio         ###   ########.fr       */
+/*   Updated: 2022/03/07 12:29:37 by dpalacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +24,6 @@ int	ft_print_bin(int int_piece, int bit_size)
 			ft_putchar('#');
 		else
 			ft_putchar('.');
-//		if ((bit_size == 16) && (i % 4 == 0))
-//			ft_putchar('\n');
 		i--;
 	}
 	return (0);
@@ -35,8 +34,7 @@ int	main(int argc, char **argv)
 	char		**file;
 	int			i; //only for printing tests
 	int			n_tet;
-	uint16_t	*piece_id;
-	t_tetri		*tetri;
+	t_tetri		*tetri_array;
 	uint16_t	map[13];
 
 	i = 0;
@@ -44,32 +42,31 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 		error(1);
 	file = read_file(argv[1], &n_tet);
-	piece_id = str_to_int(file, n_tet);
-	tetri = (t_tetri *)ft_memalloc(sizeof(t_tetri) * n_tet + sizeof(t_tetri *));
-	build_struct(n_tet, file, piece_id, tetri);
+	tetri_array = (t_tetri *)ft_memalloc(sizeof(t_tetri) * n_tet + sizeof(t_tetri *));
+	build_struct(n_tet, file, str_to_int(file, n_tet), tetri_array);
 	map_gen(map, n_tet);
 //Printing for testing purposes:
-/*	while (i < n_tet)
+	while (i < n_tet)
 	{
-		ft_print_bin((*tetri).id_int0, 16);
+		ft_print_bin((*tetri_array).id_int0, 16);
 		ft_putchar('\n');
-		ft_print_bin((*tetri).id_int1, 16);
+		ft_print_bin((*tetri_array).id_int1, 16);
 		ft_putchar('\n');
-		ft_print_bin((*tetri).id_int2, 16);
+		ft_print_bin((*tetri_array).id_int2, 16);
 		ft_putchar('\n');
-		ft_print_bin((*tetri).id_int3, 16);
+		ft_print_bin((*tetri_array).id_int3, 16);
 		ft_putchar('\n');
-//		ft_putstr((*tetri).string);
+//		ft_putstr((*tetri_array).string);
 //		ft_putstr("\t->\t");
-//		ft_putnbr((*tetri).id_int);
+//		ft_putnbr((*tetri_array).id_int);
 //		ft_putstr("\t->\t");
-//		ft_print_bin((*tetri).id_int, 16);
+//		ft_print_bin((*tetri_array).id_int, 16);
 //		ft_putstr("\t->\t");
-//		ft_putchar((*tetri).id_char);
+//		ft_putchar((*tetri_array).id_char);
 		ft_putchar('\n');
-		tetri++;
+		tetri_array++;
 		i++;
 	}
-*/	ft_putstr("It kinda works\n");
+	ft_putstr("It kinda works\n");
 	return (0);
 }
