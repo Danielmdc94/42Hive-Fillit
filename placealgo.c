73d1e67e0d78b50976c74ab6c3d97011f25ace5c
Alba Castano <acastano@gmail.com>
2022-03-09 11:34:39 +0200

    placing pieces recursively

--- placealgo.c
+++ placealgo.c
@@ -12,43 +12,69 @@
 
 #include "fillit.h"
 
-//Do not try to understand. Under construction. No sense yet
-u_int16_t	ft_place_tetri(u_int16_t *map, t_tetri *tetri)
-{
-	u_int16_t	x;
-	u_int16_t	y;
+//void	ft_update_map
 
-	x = 0;
-	y = 0;
-	while (y < 13)
+void	ft_update_tetri_xy(t_tetri *tetri, u_int16_t pos_x, u_int16_t pos_y)
+{
+  //  tetri.id_char = 'E';
+  //  tetri.string = "1000100010001000";
+  //tetri->id_int = new_id;
+  //tetri->id_int0 = ((tetri->id_int0) >> x& 61440);//F000
+  //  tetri->id_int1 = (new_id & 3840) << 4;//F00
+  //tetri->id_int2 = (new_id & 240) << 8;//F0
+  //tetri->id_int3 = (new_id & 15) << 12;//F
+  tetri->pos_x = pos_x;
+  tetri->pos_y = pos_y;
+}
+
+//Do not try to understand. Under construction. No sense yet
+u_int16_t	ft_place_tetri(u_int16_t *map, t_tetri *tetri, u_int16_t pos_x, u_int16_t pos_y)
+{
+  u_int16_t	x;
+  u_int16_t	y;
+
+  x = pos_x;//or the pieces saved pos
+  y = pos_y;//same
+  while (y < 13)
+    {
+      while (x < 13)
 	{
-		while (x < 13)
-		{
-			if (ft_collision_xy(map, tetri, x, y) == 0)
-			{
-				//update info in map and piece;
-			}
-			x++;
-		}
-		y++;
+	  if (ft_collision_xy(map, tetri, x, y) == 0)
+	    {
+	      ft_update_tetri_xy(tetri, x, y);
+	      map[y] = (map[y] | (tetri->id_int0 >> x));
+	      map[y+1] = (map[y+1] | (tetri->id_int1 >> x));
+	      map[y+2] = (map[y+2] | (tetri->id_int2 >> x));
+	      map[y+3] = (map[y+3] | (tetri->id_int3 >> x));
+	      return (1);
+	    }
+	  x++;
 	}
+      x = 0;
+      y++;
+    }
+  return (0);//didnt work
 }
 
 //Do not try to understand. Under construction. No sense yet
 u_int16_t	ft_placealgo(u_int16_t *map, t_tetri *tetris, u_int16_t n_tetris)
 {
-	u_int16_t	i;
-	u_int16_t	x;
-	u_int16_t	y;
+  u_int16_t	i;
 
-	i = 0;
-	x = 0;
-	y = 0;
-	while (x < n_tetris)
+  i = 0;
+  if (i < n_tetris)
+    {
+      if (ft_place_tetri(map, &(tetris[i])) == 1)
 	{
-		if (ft_place_tetri(map, &(tetris[x])) == 1)
-			return (1);
-		x++;
+	  i++;
+	  if (i < n_tetris)
+	    {
+	      if (ft_placealgo(map, tetris++, (n_tetris - i)) == 1)
+		return (1);
+	    }
+	  
+	  return (1);
 	}
-	return (0);//didnt work
+    }
+  return (0);//didnt work
 }
