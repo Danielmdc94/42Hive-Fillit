b4819330c3e115dcfe2c7c594379bed095be0419
Daniel Palacio <dpalacio@student.hive.fi>
2022-03-10 16:21:21 +0200

    groundwork to change tetris from int16 to int64 for optimization

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dpalacio <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 13:59:00 by dpalacio          #+#    #+#             */
-/*   Updated: 2022/03/10 13:26:40 by dpalacio         ###   ########.fr       */
+/*   Updated: 2022/03/10 16:19:24 by dpalacio         ###   ########.fr       */
 /*   Updated: 2022/03/07 12:29:37 by dpalacio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -48,16 +48,17 @@ int	main(int argc, char **argv)
   tetri_array = (t_tetri *)ft_memalloc(sizeof(t_tetri) * n_tetris + sizeof(t_tetri *));
   build_struct(n_tetris, file, str_to_int(file, n_tetris), tetri_array);
   map_size = ft_map_size(n_tetris);
-  map_gen(map, n_tetris);
+//  map_gen(map, n_tetris);
+	ft_init_map(map, map_size);
   printf("\nInitialized map\n");
   ft_print_bin_map(map);
   printf("\nPieces:\n");
-  while (i < n_tetris)
-    {
-      ft_print_tetri_16(tetri_array[i]);
-      printf("\n");
-      i++;
-    }
+//  while (i < n_tetris)
+//    {
+//      ft_print_tetri_16(tetri_array[i]);
+//      printf("\n");
+//      i++;
+//    }
   while (map_size < 13)
     {
       if (ft_placealgo(map, tetri_array, n_tetris, map_size) == 1)
